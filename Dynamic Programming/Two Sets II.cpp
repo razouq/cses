72a93e27d2ab86750d1c6bab356f2976e82365a3
anass bendarsi <bendarsi@gmail.com>
2021-02-21 10:34:15 +0100

    Two Sets II

--- Dynamic Programming/Two Sets II.cpp
+++ Dynamic Programming/Two Sets II.cpp
@@ -7,41 +7,44 @@
 #define S second
 #define PB push_back
 #define POB pop_back
-
+ 
 using namespace std;
-
+ 
 const ll MOD = 1e9+7;
-
+ 
+ll subsetSum(ll n, ll sum) {
+	vector<vector<ll>> dp(n,vector<ll>(sum+1,0));
+  	dp[0][0] = 1;
+  	for (int i = 1; i < n; i++) {
+    		for (int j = 0; j <= sum; j++) {
+      			dp[i][j] = dp[i-1][j];
+      			int left = j-i;
+      			if (left >= 0) {
+				(dp[i][j] += dp[i-1][left]) %= MOD;
+      			}
+    		}
+  	}
+  	
+  	return dp[n-1][sum];
+} 
+ 
 int main(){
 //	freopen("input.in", "r", stdin);
 //	freopen("output.out", "w", stdout);
 	ios::sync_with_stdio(0);
 	cin.tie();
 	
-	int n;
+	ll n;
 	cin>>n;
 	
-	int m = n * (n+1) / 2;
+	ll m = n * (n+1) / 2;
 	if(m % 2 != 0) cout<<0<<endl;
 	else {
+		// search the sum = m
 		m /= 2;
-		vector<ll> v(m+1, 0);
 		
-		for(int i = 1; i <= n; i++) v[i] = 1;
-		
-		for(int i = 2; i <= m; i++) {
-			for(int j = 1; j < i && i+j <= m; j++) {
-				v[i+j] += v[j];
-			}
-		}
-		
-		for(int i = 1; i <= m; i++) cout<<i<<" = "<<v[i]<<endl;
-		
-		cout<<v[m]/2<<endl;
+		cout<<subsetSum(n, m)<<endl;
 	} 
-	
-	
 	
 	return 0;
 }
-
